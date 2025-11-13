b8a6681ad50e79f9975631d87b10aa10bca2c847
Belayneh Beje <belaynehworku@gmail.com>
2025-11-12 21:16:53 -0500

    Hello world change

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ Musk's political activities, views, and statements have made him a polarizing fi
         input_variables=["information"], template=summary_template
     )
 
-    #llm = ChatOpenAI(temperature=0, model="gpt-5")
-    llm = ChatOllama(temprature=0, model="gemma3:270m")
+    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    #llm = ChatOllama(temprature=0, model="gemma3:270m")
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information":information})
     print(response.content)
